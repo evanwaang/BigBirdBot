e3eba3aba5f7d88749239a49140efc31eb210920
evan wang <evanwang@evans-MacBook-Pro.local>
2024-07-18 14:45:40 -0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 
 import { BrowserRouter } from "react-router-dom";
-import { Card } from "./entities/card";
-import Game from "./pages/Game";
+
 import { Router } from "./Router";
 
 function App() {
